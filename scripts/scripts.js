370eb578a767e1acdd678a657d644d6932c85430
Amr Mohamed <amoree.mb@gmail.com>
2022-01-30 16:45:33 +0200

    ui finally functional (might style it later)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,51 +8,56 @@ function randomPlay() {
     return play;
 } 
 
+let winCount = 0;
+let loseCount = 0;
+
+
 function playRound(computerSelection, playerSelection) {
     
+   
     playerSelection = playerSelection.toLowerCase();
     playerSelection = playerSelection.replace(playerSelection.charAt(0),playerSelection.charAt(0).toUpperCase());
     if (computerSelection === playerSelection) {
         const Tie = 'Tie';
-        return Tie;
+        document.getElementById('demo').textContent = "Tie"
     }
     else if (computerSelection === 'Rock' && playerSelection === 'Paper' || computerSelection == 'Paper' && playerSelection === 'Scissors' || computerSelection === 'Scissors' && playerSelection === 'Rock') {
         
-        return 'You won!' + " " + playerSelection + " " + 'beats ' + computerSelection ;
         
+        document.getElementById("demo").textContent = 'You won!' + " " + playerSelection + " " + 'beats ' + computerSelection ;;
+        winCount++
+        document.getElementById('player-score').textContent = `You: ${winCount}`
     }
     else {
-        
-        return 'Git Gud you lost nub.' + " " + computerSelection + " " + 'beats ' + playerSelection;
+        document.getElementById("demo").textContent ='Git Gud you lost nub.' + " " + computerSelection + " " + 'beats ' + playerSelection;
+        loseCount++
+        document.getElementById('comp-score').textContent = `Computer: ${loseCount}`
     }
+        
+    if (winCount === 5) {
+        alert('You won NOICEEEEEE')
+        winCount = 0
+        loseCount = 0
+        document.getElementById('player-score').textContent = `You: ${winCount}`
+        document.getElementById('comp-score').textContent = `Computer: ${loseCount}`
+    }
+    else if (loseCount === 5) {
+        alert("SADDDD you lose")
+        winCount = 0
+        loseCount = 0
+        document.getElementById('player-score').textContent = `You: ${winCount}`
+        document.getElementById('comp-score').textContent = `Computer: ${loseCount}`
+    }
+}
 
-}
-function game() {
-    let i = 0;
-    let winCount = 0;
-    let loseCount = 0;
-    let userPlay;
-    while (i < 5) {
-        userPlay = prompt('Choose your weapon','rock');
-        let result = playRound(computerPlay(),userPlay);
-        console.log(result);
-        i++;
-        if (result.includes("won")) {
-            winCount++
-        
-        }
-        else if (result.includes("lost")) {
-            loseCount++
-        }
-        
-    }
-    if (winCount > loseCount) {
-        return `You won ${winCount} to ${loseCount}`
-    }
-    else if (winCount < loseCount) {
-        return `You lost ${loseCount} to ${winCount}`
-    }
-    else {
-        return `You tied ${loseCount} to ${winCount}`
-    }
-}
+
+const rockBtn = document.querySelector('.rock');
+rockBtn.addEventListener('click',function(){ playRound(computerPlay(),'rock'); });
+
+const paperBtn = document.querySelector('.paper');
+paperBtn.addEventListener('click',function(){ playRound(computerPlay(),'paper'); });
+
+const scissorsBtn = document.querySelector('.scissors');
+scissorsBtn.addEventListener('click',function(){ playRound(computerPlay(),'scissors'); });
+
+
